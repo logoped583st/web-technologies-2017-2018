716fe120953a9a4bfec4ad2823c7468ffd077e2f
logoped583st <logoped583st@mail.ru>
2018-06-16 16:50:57 +0300

    create base folder architecture, add redux

---
+++ lab4/reducers/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    image: '',
+    name: '',
+    login: '',
+    bio: '',
+    blog: '',
+    company: '',
+    location: '',
+    email: '',
+    social: ''
+};
+
+
+
+export default reducer;
